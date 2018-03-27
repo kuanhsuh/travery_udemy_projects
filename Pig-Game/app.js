25fc1ecee5dfbf2ef395329df72fb3918a774643
Danny Huang <kuanhsuh@gmail.com>
2018-03-27 22:01:39 +0800

    Finish 43

--- Pig-Game/app.js
+++ Pig-Game/app.js
@@ -11,22 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer
 
-scores = [0,0]
-roundScore = 0
-activePlayer = 0
-
-
-//document.querySelector('#current-' + activePlayer).textContent = dice
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
-
-// var x = document.querySelector('#score-0').textContent
-// console.log(x)
-
-document.querySelector('.dice').style.display = 'none'
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
+init()
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     // 1. Random Number
@@ -73,4 +58,25 @@ function nextPlayer () {
     document.querySelector('.player-1-panel').classList.toggle('active')
 
     document.querySelector('.dice').style.display = 'none'
+}
+
+document.querySelector('.btn-new').addEventListener('click', init)
+
+function init () {
+    scores = [0,0]
+    activePlayer = 0
+    roundScore = 0
+
+    document.querySelector('.dice').style.display = 'none'
+    document.getElementById('score-0').textContent = '0'
+    document.getElementById('score-1').textContent = '0'
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+    document.querySelector('#name-0').textContent = 'Player 1'
+    document.querySelector('#name-1').textContent = 'Player 2'
+    document.querySelector('.player-0-panel').classList.remove('winner')
+    document.querySelector('.player-1-panel').classList.remove('winner')
+    document.querySelector('.player-0-panel').classList.remove('active')
+    document.querySelector('.player-1-panel').classList.remove('active')
+    document.querySelector('.player-0-panel').classList.add('active')
 }
