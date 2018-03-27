912fb3856a6d4b6cc8dcd3066491b3ac93fe04b1
Danny Huang <kuanhsuh@gmail.com>
2018-03-27 22:08:53 +0800

    finish #44

--- Pig-Game/app.js
+++ Pig-Game/app.js
@@ -9,43 +9,47 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer
+var scores, roundScore, activePlayer, gamePlaying
 
 init()
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-    // 1. Random Number
-    var dice = Math.floor((Math.random() * 6) + 1)
-
-    // 2. Display Result
-    var diceDOM = document.querySelector('.dice')
-    diceDOM.style.display = 'block'
-    diceDOM.src = 'dice-' + dice + '.png'
-    // 3. Update the round score If rolled number was not a 1
-    if(dice !== 1) {
-        //  Add score
-        roundScore += dice
-        document.querySelector('#current-' + activePlayer).textContent = roundScore
-    } else {
-        // Next player
-        nextPlayer()
+    if (gamePlaying) {
+        // 1. Random Number
+        var dice = Math.floor((Math.random() * 6) + 1)
+        // 2. Display Result
+        var diceDOM = document.querySelector('.dice')
+        diceDOM.style.display = 'block'
+        diceDOM.src = 'dice-' + dice + '.png'
+        // 3. Update the round score If rolled number was not a 1
+        if(dice !== 1) {
+            //  Add score
+            roundScore += dice
+            document.querySelector('#current-' + activePlayer).textContent = roundScore
+        } else {
+            // Next player
+            nextPlayer()
+        }
     }
 })
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-    // Add current score to global score
-    scores[activePlayer] += roundScore
-    // update UI
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
-    // Check if player won the game
-    if (scores[activePlayer] >= 20) {
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
-        document.querySelector('.dice').style.display = 'none'
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
-    } else {
-        // Next Player
-        nextPlayer()
+    if(gamePlaying) {
+        // Add current score to global score
+        scores[activePlayer] += roundScore
+        // update UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+        // Check if player won the game
+        if (scores[activePlayer] >= 20) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
+            document.querySelector('.dice').style.display = 'none'
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+            gamePlaying = false
+        } else {
+            // Next Player
+            nextPlayer()
+        }
     }
 })
 
@@ -66,6 +70,7 @@ function init () {
     scores = [0,0]
     activePlayer = 0
     roundScore = 0
+    gamePlaying = true
 
     document.querySelector('.dice').style.display = 'none'
     document.getElementById('score-0').textContent = '0'
